1b735a5d7f1f10cc93a049cd2d9a2b6c4db14ad5
Knyaz Qasan <87781961+KnyazQasan@users.noreply.github.com>
2021-09-23 17:18:47 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>document</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="container">
+		<form action="php/create.php" 
+		      method="post">
+            
+		   <h4 class="display-4 text-center">Elave Et</h4><hr><br>
+		   <?php if (isset($_GET['error'])) { ?>
+		   <div class="alert alert-danger" role="alert">
+			  <?php echo $_GET['error']; ?>
+		    </div>
+		   <?php } ?>
+		   <div class="form-group">
+		     <label for="name">AD</label>
+		     <input type="name" 
+		           class="form-control" 
+		           id="name" 
+		           name="name" 
+		           value="<?php if(isset($_GET['name']))
+		                           echo($_GET['name']); ?>" 
+		           placeholder="Ad daxil Edin">
+		   </div>
+
+		   <div class="form-group">
+		     <label for="email">Elektron Pocht</label>
+		     <input type="email" 
+		           class="form-control" 
+		           id="email" 
+		           name="email" 
+		           value="<?php if(isset($_GET['email']))
+		                           echo($_GET['email']); ?>"
+		           placeholder="E mail Daxil Edin">
+		   </div>
+
+		   <button type="submit" 
+		          class="btn btn-primary"
+		          name="create">Daxil Et</button>
+		    <a href="read.php" class="link-primary">Elave Olunanlara Bax</a>
+	    </form>
+	</div>
+</body>
+</html>
